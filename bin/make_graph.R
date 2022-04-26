ecd56f92dc9736d4592247059cd43c2b0beeed24
Eskimuz <93330690+Eskimuz@users.noreply.github.com>
2022-04-26 02:01:02 +0200

    Update make_graph.R

--- bin/make_graph.R
+++ bin/make_graph.R
@@ -192,4 +192,104 @@ pdf(output_plot2)
 print(wealth)
 dev.off()
 
+gdp<- outputs %>%
+  group_by(step) %>%
+  summarise_at(vars(`GDP-spending`),list(GDP= mean))
 
+ gdpshades<- outputs %>%
+  group_by(step) %>%
+  summarize(highgdp = quantile(`GDP-spending`, probs = 0.975),
+            lowgdp = quantile(`GDP-spending`, probs = 0.025))
+ gdp <- merge(gdp,gdpshades,by="step")
+
+goods<- outputs %>%
+  group_by(step) %>%
+  summarise_at(vars(`goods-income-value`),list(goodsvalue= mean))
+
+goodsshape<- outputs %>%
+  group_by(step) %>%
+  summarize(highgoods = quantile(`goods-income-value`, probs = 0.975),
+            lowgoods = quantile(`goods-income-value`, probs = 0.025))
+ 
+ goods<- merge(goods,goodsshape,by="step")
+
+land<- outputs %>%
+  group_by(step) %>%
+  summarise_at(vars(`land-income-value`),list(landvalue= mean))
+
+landshapes<- outputs %>%
+  group_by(step) %>%
+  summarize(highland = quantile(`land-income-value`, probs = 0.975),
+            lowland = quantile(`land-income-value`, probs = 0.025))
+
+land<- merge(land,landshapes,by="step")
+
+labor<- outputs %>%
+  group_by(step) %>%
+  summarise_at(vars(`labor-income-value`),list( laborvalue = mean))
+
+laborshapes<- outputs %>%
+  group_by(step) %>%
+  summarize(highlabor = quantile(`labor-income-value`, probs = 0.975),
+            lowlabor = quantile(`labor-income-value`, probs = 0.025))
+
+labor<- merge(labor,laborshapes,by="step")
+
+
+service<- outputs %>%
+  group_by(step) %>%
+  summarise_at(vars(`service-income-value`),list(servicevalue = mean))
+
+serviceshades<- outputs %>%
+  group_by(step) %>%
+  summarize(highservice = quantile(`service-income-value`, probs = 0.975),
+            lowservice = quantile(`service-income-value`, probs = 0.025))
+
+service<- merge(service,serviceshades,by="step")
+
+profits<- outputs %>%
+  group_by(step) %>%
+  summarise_at(vars(`profit-income-value`),list(profitvalue= mean))
+
+profitsshades<- outputs %>%
+  group_by(step) %>%
+  summarize(highprofits = quantile(`profit-income-value`, probs = 0.975),
+            lowprofits = quantile(`profit-income-value`, probs = 0.025))
+
+profits<- merge(profits,profitsshades,by="step")
+
+
+forgdp <- merge(gdp,goods,by="step")
+forgdp <- merge(forgdp,land,by="step")
+forgdp <- merge(forgdp,labor,by="step")
+forgdp <- merge(forgdp,service,by="step")
+forgdp <- merge(forgdp,profits,by="step")
+
+
+
+forGDP <- ggplot(data = forgdp, aes(x=step)) + ##produces the plot
+  geom_line(aes(y=GDP,color='GDP')) +
+  geom_line(aes(y=goodsvalue,color='Goods')) +
+  geom_line(aes(y=landvalue,color='Food')) +
+  geom_line(aes(y=servicevalue,color='Services')) +
+  geom_line(aes(y=profitvalue,color='Profits')) +
+  geom_line(aes(y=laborvalue,color='Labor')) +
+  geom_ribbon(data = forgdp,aes(x=step,y=GDP,ymin = lowgdp, ymax = highgdp), alpha=0.1) +
+  geom_ribbon(data = forgdp,aes(x=step,y=goodsvalue,ymin = lowgoods, ymax = highgoods), alpha=0.1) +
+  geom_ribbon(data = forgdp,aes(x=step,y=landvalue,ymin = lowland, ymax = highland), alpha=0.1) +
+  geom_ribbon(data = forgdp,aes(x=step,y=servicevalue,ymin = lowservice, ymax = highservice), alpha=0.1) +
+  geom_ribbon(data = forgdp,aes(x=step,y=profitvalue,ymin = lowprofits, ymax = highprofits,), alpha=0.5) +
+  geom_ribbon(data = forgdp,aes(x=step,y=laborvalue,ymin = lowlabor, ymax = highlabor,), alpha=0.5) +
+  labs(x = 'Time', y = 'Value' ) + #changes the plot to a line
+  ggtitle('GDP and sectors contributions ') +
+  scale_color_manual(name = "Classes", values = c("GDP"="black","Services" = "orange", "Labor" = "red","Profits" = "blue", "Food" = "green", "Goods" = "yellow"))+
+  geom_vline(xintercept = 100) + #line for time change
+  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+
+forGDP
+
+
+pdf(output_plot3)
+print(forGDP)
+dev.off()
